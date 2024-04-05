dbb79cb7198a4af1a583965dee4430560f2b541e
dylanvo1203 <165742961+dylanvo1203@users.noreply.github.com>
2024-04-04 20:36:01 -0400

    Add files via upload

---
+++ proposal_vo.py
@@ -0,0 +1,43 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+# CIS 1051 Final Project - vo
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+# A program that is similar to the popular Cookie Clicker game. I want to see how far I can go with upgrades/multipliers in the game.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+# A cookie will be in the center of the screen and the player can press on this cookie to increase the cookie count. The collected cookies can be used to
+# purchase upgrades that can help the player earn more cookies faster. I was recommonded to use Unity and C#, so I will be learning those two before going
+# into making the game.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+# N/A
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+# N/A
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+# A game where the player can click on a cookie to earn more cookies.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+# A game where the player can click on a cookie to earn more cookies and use those cookies to buy upgrades to gain even more cookies.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+# A game where the player can click on a cookie to earn more cookies and use those cookes to buy upgrades to gain even more cookies. Addtionally,
+# a prestige feature could be added so the player can restart with a cookie multiplier.
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+# Since I am using Unity and C#, I will have to quickly learn the necessary information to begin this project, since I have never used either before.
+
